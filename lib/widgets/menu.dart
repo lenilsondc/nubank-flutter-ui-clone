692022ea08b6f940d73994e2e13e40279ee45c98
lenilsondc <lenilson.decastro@yahoo.com>
2019-09-13 16:05:29 -0300

    feat: add scroll view to make the menu scrollable as it grows

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -3,43 +3,45 @@ import 'package:flutter/material.dart';
 class AppMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 30),
-        decoration: BoxDecoration(
-            border:
-                Border(top: BorderSide(color: Colors.white.withOpacity(.8)))),
-        child: Column(
-          children: <Widget>[
-            AppMenuItem(text: 'Me ajuda', icon: Icons.help_outline),
-            AppMenuItem(text: 'Perfil', icon: Icons.person_outline),
-            AppMenuItem(text: 'Configurar cartão', icon: Icons.credit_card),
-            AppMenuItem(text: 'Configurações do app', icon: Icons.smartphone),
-            Container(
-              margin: EdgeInsets.only(top: 15),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(
-                                color: Colors.white.withOpacity(.8))),
-                        child: Text(
-                          'SAIR DO APP',
-                          style: TextStyle(color: Colors.white),
+    return SingleChildScrollView(
+      child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+              border:
+                  Border(top: BorderSide(color: Colors.white.withOpacity(.8)))),
+          child: Column(
+            children: <Widget>[
+              AppMenuItem(text: 'Me ajuda', icon: Icons.help_outline),
+              AppMenuItem(text: 'Perfil', icon: Icons.person_outline),
+              AppMenuItem(text: 'Configurar cartão', icon: Icons.credit_card),
+              AppMenuItem(text: 'Configurações do app', icon: Icons.smartphone),
+              Container(
+                margin: EdgeInsets.only(top: 15),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              border: Border.all(
+                                  color: Colors.white.withOpacity(.8))),
+                          child: Text(
+                            'SAIR DO APP',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ));
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
 
