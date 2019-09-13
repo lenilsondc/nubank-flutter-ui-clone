76fa9cf3f29359692b7e01034dc1d9253c4def0a
lenilsondc <lenilson.decastro@yahoo.com>
2019-09-13 18:34:44 -0300

    feat: add header margin bottom to separate the header top from the arrow down icon

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -4,24 +4,27 @@ class AppHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 40, bottom: 30),
+      padding: EdgeInsets.only(top: 80, bottom: 30),
       child: Column(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/images/Nubank_Logo.png'),
-              SizedBox(
-                width: 8,
-              ),
-              Text(
-                'Lenilson',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/Nubank_Logo.png'),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  'Lenilson',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
           ),
           Icon(
             Icons.keyboard_arrow_down,
