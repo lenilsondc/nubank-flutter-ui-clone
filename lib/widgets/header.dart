fa2889decd0e7b86fb6c05bdf69376b93ae38f83
lenilsondc <lenilson.decastro@yahoo.com>
2019-09-13 14:39:49 -0300

    feat: add padding and arrow icon to header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
-class Header extends StatelessWidget {
+class AppHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: EdgeInsets.only(top: 40, bottom: 30),
+      child: Column(
         children: <Widget>[
-          Image.asset('assets/images/Nubank_Logo.png'),
-          SizedBox(
-            width: 8,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/images/Nubank_Logo.png'),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                'Lenilson',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
           ),
-          Text(
-            'Lenilson',
-            style: TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 20,
+            color: Colors.white,
           )
         ],
       ),
