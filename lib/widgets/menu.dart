e1a8b12bb08daf16f2ff5fab443ef28c20b28196
lenilsondc <lenilson.decastro@yahoo.com>
2019-09-16 10:40:20 -0300

    feat: add menu qrcode

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class AppMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 30),
-          decoration: BoxDecoration(
-              border:
-                  Border(top: BorderSide(color: Colors.white.withOpacity(.8)))),
-          child: Column(
-            children: <Widget>[
-              AppMenuItem(text: 'Me ajuda', icon: Icons.help_outline),
-              AppMenuItem(text: 'Perfil', icon: Icons.person_outline),
-              AppMenuItem(text: 'Configurar cartão', icon: Icons.credit_card),
-              AppMenuItem(text: 'Configurações do app', icon: Icons.smartphone),
-              AppSignOutButton()
-            ],
-          )),
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(vertical: 30),
+            child: QrImage(
+              data: "https://nubank.com.br",
+              version: QrVersions.auto,
+              size: 80.0,
+              foregroundColor: Colors.white,
+              backgroundColor: Color(0xff8B10AE),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                  border: Border(
+                      top: BorderSide(color: Colors.white.withOpacity(.8)))),
+              child: Column(
+                children: <Widget>[
+                  AppMenuItem(text: 'Me ajuda', icon: Icons.help_outline),
+                  AppMenuItem(text: 'Perfil', icon: Icons.person_outline),
+                  AppMenuItem(
+                      text: 'Configurar cartão', icon: Icons.credit_card),
+                  AppMenuItem(
+                      text: 'Configurações do app', icon: Icons.smartphone),
+                  AppSignOutButton()
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
